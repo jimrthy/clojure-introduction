e176138dc247b8e583284c0b717f11f804ed390a
James Gatannah <james.gatannah@gmail.com>
2016-09-04 21:14:06 -0500

    Switch to lein managed-dependencies
    deps :tree shows some version conflicts with my profile, but this gets
    cider working again.

--- usage/project.clj
+++ usage/project.clj
@@ -4,4 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [#_[org.clojure/clojure "1.9.0-alpha10"]
-                 [cider-transitive-version "0.1.0-SNAPSHOT"]])
+                 [org.clojure/clojure]
+                 #_[cider-transitive-version "0.1.0-SNAPSHOT"]]
+  :parent-project {:path "../cider-transitive-version/project.clj"
+                   :inherit [:managed-dependencies]}
+  :plugins [[lein-parent "0.3.0"]])
